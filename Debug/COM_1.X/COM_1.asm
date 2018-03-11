72f9e4f7f028f3ab373486231bac71e7313b73b7
Keanu Kent Gargar <keanukentgargar@outlook.com>
2018-03-11 16:00:12 +0800

    Code and File Structure improvement
    Seperated all module initialization into include files.

--- Debug/COM_1.X/COM_1.asm
+++ Debug/COM_1.X/COM_1.asm
@@ -26,8 +26,6 @@
     CBLOCK 0x20
 	temp	
 	delay_count
-	var1
-	TableLength1
     ENDC
 
    
@@ -49,6 +47,7 @@ head:
     call initLCD
 main:
 ;    MAGIC!
+    
 initTable:
     movlw  UPPER stringDB
     movwf  TBLPTRU
@@ -67,60 +66,24 @@ readDB:
     movf TABLAT, W	    ; get data
     btfsc STATUS, Z	    ; zero if end of message
     return
-    movwf temp		    ; store receive byte
-    call write_mode	    ; set lcd transmission to write mode
-    movf temp, W	    ; pop receive byte
-    call write_lcd	    ; Output to console
-    call delay_5ms
+    call output_lcd	    ; Output to console
+;    call delay_5ms
     goto readDB		    ; Next char
-    
     
     goto main
     
-initInterrupt:
-    bcf INTCON, INT0IF	    ; clear ext. int. flag bit (RB0)
-    bsf INTCON, INT0IE	    ; enable ext. int. (RB0)
-    bsf PIE1, RCIE	    ; enable receive UART int. flag bit (RX)
-    bsf INTCON, PEIE	    ; enable periphal int. (TX/RX)
-    bsf INTCON, GIE	    ; enable global int. (All Interrupt bits)
     
-    return
+; <------- STRING DB -------> ;
     
-interrupt_service_routine:
-    btfss INTCON, INT0IF    ; check if RB0 triggered the interrupt or not
-    goto int_uart	    ; if RB0 did not, it's the RX UART
-    btg LATA, 1
-    movlw 0x61		    ; send 'a'
-    call writeUART	    ; write to UART
-isr_stop:
-    bcf INTCON, INT0IF	    ; clear (RB0) interrupt flag bit
-    retfie		    ; return to current prog.
+stringDB:
+    db "Hello World!", 0
+bootUpDB:
+    db "Queue System!", 0
+
+; </------ STRING DB ------/> ;    
     
-int_uart:
-    btg LATA, 0
-    call readUART	    
-    movwf temp		    ; store receive byte
-    call write_mode	    ; set lcd transmission to write mode
-    movf temp, W	    ; pop receive byte
-    call write_lcd
-    movf temp, W
-    call writeUART
-    bcf INTCON, INT0IF
-    goto isr_stop
     
-initUART:
-    bcf TXSTA, TXEN	    ; disable UART transmission
-    bcf TXSTA, SYNC	    ; set as Asynchronous 
-    bsf TXSTA, BRGH	    ; use high-speed mode
-    movlw d'25'		    ; config baud rate as 9600
-    movwf SPBRG		    ; set baud rate
-    bsf RCSTA, SPEN	    ; enable serial port (TX/RX)
-    return
-    
-initTX:
-    bcf TXSTA, TX9	    ; use 8-bit transmission
-    bsf TXSTA, TXEN	    ; enable UART transmission
-    return
+; <------- INITIALIZATION -------> ;
     
 initPort:
     movlw 0X06
@@ -134,125 +97,13 @@ initPort:
     
     clrf TRISD
     return
+    
+#INCLUDE <interrupt.inc>
+#INCLUDE <uart.inc>  
+#INCLUDE <lcd.inc>
+#INCLUDE <delay.inc>  
 
-initRX:
-    bcf RCSTA, RX9	    ; use 8-bit receiver
-    bsf RCSTA, CREN	    ; enable continuous receive
-    return
+; </------ INITIALIZATION ------/> ;
     
-writeUART:
-    movwf TXREG		    ; register buffer to transmit
-checkTXIF:
-    btfss PIR1, TXIF	    ; check if all data has been transmitted
-    goto checkTXIF
-    return
-    
-readUART:
-    btfss PIR1, RCIF	    ; check if all data has been received
-    goto readUART
-    movf RCREG, W	    ; move received data to W reg
-    return
-
-displayLCD:
-    call command_mode
-    
-    movlw 0x80
-    call write_lcd
-    
-    call write_mode
-    
-    movlw 0x41
-    call write_lcd
-    
-    movlw 0x42
-    call write_lcd
-    
-    call command_mode
-    
-    movlw 0xC0 
-    call write_lcd
-    
-    call write_mode
-    
-    movlw 0x41
-    call write_lcd
-    
-    movlw 0x42
-    call write_lcd
-    
-    return
-    
-    
-initLCD:
-    call command_mode
-    call delay_5ms
-    
-    movlw b'00000001' ; Clearing display
-    call write_lcd
-
-    movlw b'00111000' ; Funtion set
-    call write_lcd
-
-    movlw b'00001111' ; Display on off
-    call write_lcd
-
-    movlw b'00000110' ; Entry mod set blinking
-    call write_lcd
-    
-    movlw 0x38
-    call write_lcd
-    
-    call delay_5ms
-    
-    return
-    
-Enable: 
-    bsf LATC,1 ; E pin is high, (LCD is processing the incoming data)
-    nop
-    bcf LATC,1 ; E pin is low, (LCD does not care what is happening)
-    call delay_5ms
-    
-    return
-
-command_mode:
-    bcf LATC, 0 ; Setting RS as 0 (Sends commands to LCD)
-    call delay_5ms
-    
-    return
-
-write_mode:
-    bsf LATC, 0 ; Setting RS as 1 (Sends commands to LCD)
-    call delay_5ms
-    
-    return
-    
-write_lcd:
-    movwf LATD 
-    call Enable
-    
-    return
-    
-delay_5ms: 
-    movlw 0xff
-    movwf delay_count
-count:
-    nop
-    decfsz delay_count
-    goto count
-    return    
-    
-stringDB:
-    db     "Hello World!", 0
-    db	    "Test", 0
-    
- 	
-TableRead1:
- 	addwf	PCL, F
-	dt "Hi", 0x00
-; 	retlw	'H'
-; 	retlw	'e'
-; 	retlw	'l'
-; 	retlw	'l'
-; 	retlw	'o'
     
     END
